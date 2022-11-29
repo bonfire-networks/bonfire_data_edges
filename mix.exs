878a20b364e29336225bb98f751d5e7a1be5522c
Mayel de Borniol <git@mayel.space>
2022-11-29 14:20:56 +1300

    Function as multi repo umbrella

--- mix.exs
+++ mix.exs
@@ -1,7 +1,20 @@
+Code.eval_file("mess.exs", (if File.exists?("../../lib/mix/mess.exs"), do: "../../lib/mix/"))
+
 defmodule Bonfire.Data.Edges.MixProject do
   use Mix.Project
 
   def project do
+    if File.exists?("../../.is_umbrella.exs") do
+      [
+        build_path: "../../_build",
+        config_path: "../../config/config.exs",
+        deps_path: "../../deps",
+        lockfile: "../../mix.lock"
+      ]
+    else
+      []
+    end
+    ++
     [
       app: :bonfire_data_edges,
       version: "0.1.0",
@@ -16,7 +29,7 @@ defmodule Bonfire.Data.Edges.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
+    Mess.deps [
       # {:pointers, "~> 0.5.0"}
       {:pointers,
        git: "https://github.com/bonfire-networks/pointers.git", branch: "main"}
